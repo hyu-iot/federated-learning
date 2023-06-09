2e24a167836df36a0eafd26db562d89ebf7929e9
Jakio815 <jakio@hanyang.ac.kr>
2023-06-09 16:58:01 +0900

    Add load_data apis

---
+++ final/flower_client.py
@@ -0,0 +1,80 @@
+from collections import OrderedDict
+from typing import List, Tuple, Dict, Optional
+import numpy as np
+import torch
+import flwr as fl
+from flwr.common import Metrics
+
+def get_parameters(net) -> List[np.ndarray]:
+    return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+def set_parameters(net, parameters: List[np.ndarray]):
+    params_dict = zip(net.state_dict().keys(), parameters)
+    state_dict = OrderedDict(
+        {
+            k: torch.Tensor(v) if v.shape != torch.Size([]) else torch.Tensor([0])
+            for k, v in params_dict
+        }
+    )
+    net.load_state_dict(state_dict, strict=True)
+
+# The `evaluate` function will be by Flower called after every round
+def evaluate(
+    server_round: int,
+    parameters: fl.common.NDArrays,
+    config: Dict[str, fl.common.Scalar],
+) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
+    # net = Net().to(DEVICE)
+    set_parameters(net, parameters)  # Update model with the latest parameters
+    loss, metrics = test(net, full_testloader) # Test total test_loader
+    print(f"Server-side evaluation loss {loss} / accuracy {metrics['accuracy']}")
+    return loss, metrics
+
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self, net, trainloader, valloader):
+        self.net = net
+        self.trainloader = trainloader
+        self.valloader = valloader
+
+    def get_parameters(self, config):
+        return get_parameters(self.net)
+
+    def fit(self, parameters, config):
+        set_parameters(self.net, parameters)
+        train(self.net, self.trainloader, epochs=1)
+        return get_parameters(self.net), len(self.trainloader), {}
+
+    def evaluate(self, parameters, config):
+        set_parameters(self.net, parameters)
+        loss, metrics = test(self.net, self.valloader)
+        print(f"Accuracy {metrics['accuracy']}")
+        return float(loss), len(self.valloader), metrics
+    
+def client_fn(cid: str) -> FlowerClient:
+    """Create a Flower client representing a single organization."""
+
+    # Load model
+    # net = Net().to(DEVICE)
+
+    # Load data (CIFAR-10)
+    # Note: each client gets a different trainloader/valloader, so each client
+    # will train and evaluate on their own unique data
+    trainloader = trainloaders[int(cid)]
+    valloader = valloaders[int(cid)]
+
+    # Create a  single Flower client representing a single organization
+    return FlowerClient(net, trainloader, valloader)
+    
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+
+    averages = {}
+    targets = ["accuracy", "recall", "precision", "f1_score"]
+
+    total_examples = sum([num_examples for num_examples, _ in metrics])
+    for target in targets:
+        target_distributed = [num_examples * m[target] for num_examples, m in metrics]
+        averages[target] = sum(target_distributed) / total_examples
+    
+    # Aggregate and return custom metric (weighted average)
+    return averages
+
---
+++ final/load_data.py
@@ -0,0 +1,63 @@
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import CIFAR10, CIFAR100
+
+# Loads the dataset's train set and test set. The resulted train set is the raw train set, without splitting to the validation set.
+def get_full_dataset(config):
+    dataset_mean = {
+        'cifar10': (0.4914, 0.4822, 0.4465),
+        'cifar100': (0.5071, 0.4865, 0.4409),
+    }
+    dataset_std = {
+        'cifar10': (0.2023, 0.1994, 0.2010),
+        'cifar100': (0.2673, 0.2564, 0.2762),
+    }
+    if config.dataset == 'CIFAR-10':
+        transform = transforms.Compose(
+            [transforms.ToTensor(), 
+             transforms.Normalize(dataset_mean['cifar10'], dataset_std['cifar10'])
+            ])
+        full_trainset = CIFAR10("../dataset", train=True, download=True, transform=transform)
+        testset = CIFAR10("../dataset", train=False, download=True, transform=transform)
+
+    if config.dataset == 'CIFAR-100':
+        transform = transforms.Compose(
+            [transforms.ToTensor(), 
+             transforms.Normalize(dataset_mean['cifar10'], dataset_std['cifar100'])
+            ])
+        full_trainset = CIFAR100("../dataset", train=True, download=True, transform=transform)
+        testset = CIFAR100("../dataset", train=False, download=True, transform=transform)
+    return full_trainset, testset
+
+def split_validation_set(config, full_trainset):
+    lengths = [len(full_trainset) * (1 - config.dataset.validation_percentage), len(full_trainset) * config.dataset.validation_percentage]
+    split_trainset, valset = random_split(full_trainset, lengths) # TODO: maybe add seed option? 
+    return split_trainset, valset
+
+def get_full_trainloader(config, full_train_set):
+    return DataLoader(full_train_set, batch_size=config.batch_size, shuffle=True) #TODO: config.batch_size
+
+def get_splitted_trainloader(config, splitted_train_set):
+    return DataLoader(splitted_train_set, batch_size=config.batch_size, shuffle=True) #TODO: config.batch_size
+
+def get_valloader(config, val_set):
+    return DataLoader(val_set, batch_size=config.batch_size, shuffle=True) #TODO: config.batch_size
+
+def get_testloader(config, test_set):
+    return DataLoader(test_set, batch_size=config.batch_size, shuffle=True) #TODO: config.batch_size
+
+def get_federated_client_dataloaders(config, full_train_set):
+    # Split training set into the number of the clients to simulate the individual dataset
+    partition_size = len(full_train_set) // config.num_clients #TODO: check config
+    lengths = [partition_size] * config.num_clients #TODO: check config
+    datasets= random_split(full_train_set, lengths) # TODO: maybe add seed option? 
+
+    # Split each partition into train/val and create DataLoader
+    trainloaders = []
+    valloaders = []
+    for idx, ds in enumerate(datasets):
+        lengths = [len(ds) * (1 - config.dataset.validation_percentage), len(ds) * config.dataset.validation_percentage ] #TODO: check config
+        ds_train, ds_val = random_split(ds, lengths)
+        trainloaders.append(DataLoader(ds_train, batch_size=config.batch_size, shuffle=True)) #TODO: config.batch_size
+        valloaders.append(DataLoader(ds_val, batch_size=config.batch_size)) #TODO: config.batch_size
+    return trainloaders, valloaders
