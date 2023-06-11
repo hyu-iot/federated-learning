4a8631b3dc23cda7d6569217fc78ea3c5fb687de
Jakio815 <jakio@hanyang.ac.kr>
2023-06-11 17:03:12 +0900

    Fix custom_model bug

--- models/custom_model.py
+++ models/custom_model.py
@@ -4,17 +4,21 @@ from torch import nn
 import torch.nn.functional as F
 
 class CustomModel(nn.Module):
-  def __init__(self):
-    super(CustomModel, self).__init__()
-    self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=5, stride=1)
-    self.conv2 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=5, stride=1)
-    self.fc1 = nn.Linear(10 * 12 * 12, 50)
-    self.fc2 = nn.Linear(50, 10)
-  
-  def forward(self, x):
-    x = F.relu(self.conv1(x))
-    x = F.relu(self.conv2(x))
-    x = x.view(-1, 10 * 12 * 12)
-    x = F.relu(self.fc1(x))
-    x = self.fc2(x)
-    return x
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
